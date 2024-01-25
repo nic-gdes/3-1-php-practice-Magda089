2fee5e5d03f803fa25351451308253b2ffbac934
Magdalena Idzikowska <midzikowska@students.nic.edu>
2024-01-24 18:34:44 -0800

    FORM CREATED
    Mostly done

--- components/nav.php
+++ components/nav.php
@@ -4,7 +4,7 @@
             <div class="logo"><a href="https://www.wilddesigndev.com/"><img src="../pictures/wild-design-rebranding-white-covered.png" alt="Wild Design" /></a></div>
             <div class="nav-links">
                 <ul>
-                    <li><a href="#">Home</a></li>
+                    <li><a href="../index.php">Home</a></li>
                     <li><a href="./contact.php">Contact</a></li>
                 </ul>
             </div>
--- contact.php
+++ contact.php
@@ -1,6 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == ){
+$page_title = "Home";
+include('./components/head.php');
+?>
+</head>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
@@ -10,13 +19,44 @@ if ($_SERVER["REQUEST_METHOD"] == ){
 ?>
 
 <?php
-if (isset ($_POST['submit'])) {
-    
+if (isset($_POST['submit'])) {
 }
 ?>
 
 
 <body>
-    <h1> Contact </h1>
+    <?php
+    include('./components/nav.php');
+    ?>
+
+    <main>
+        <h1> Contact </h1>
+        <h2>Fill the form so I can contact you!</h2>
+        <section class="form">
+            <form action="./contact.php" method="POST">
+                <div class="input-group">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name">
+                    <?php if ($name == "") {
+                        echo "<h2>This is required</h2>";
+                    } ?>
+                </div>
+                <div class="input-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email">
+                </div>
+                <div class="input-group">
+                    <label for="message">Your message</label>
+                    <textarea name="message" id="message" rows="10"></textarea>
+                </div>
+                <div class="input-submit"><input type="submit" value="Submit" /></div>
+            </form>
+
+        </section>
+    </main>
+    <?php
+    include('./components/footer.php');
+    ?>
 </body>
+
 </html>
